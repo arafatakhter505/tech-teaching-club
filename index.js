a563e26f6381c78a1dfac81417ec1b165585033a
arafatakhter505 <arafatakhterforhad@gmail.com>
2022-10-25 16:42:27 +0600

    api create

--- index.js
+++ index.js
@@ -6,13 +6,32 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require("./data/categories.json");
+const courses = require("./data/courses.json");
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Tech Teaching Club Server Side");
 });
 
 app.get("/categories", (req, res) => {
   res.send(categories);
+});
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/courses/:categorie", (req, res) => {
+  const categorie = req.params.categorie;
+  const filterCourses = courses.filter(
+    (course) => course.categorie === categorie
+  );
+  res.send(filterCourses);
+});
+
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const findCourse = courses.find((course) => course.id === id);
+  res.send(findCourse);
 });
 
 app.listen(port, () => {
